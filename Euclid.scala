ac0c77775910d7364bd34506143961c5fda4d827
Tomoya Kobayashi <tomocha.marika@gmail.com>
2021-05-01 15:42:50 +0900

    advanced level

---
+++ Euclid.scala
@@ -0,0 +1,12 @@
+object Euclid {
+
+ def greatestCommonDivisor(a: Int, b: Int): Int = {
+     if (a == 0) {
+         return b;
+     } else if (b == 0) {
+         return a;
+     } else {
+         return greatestCommonDivisor(b, a % b);
+     }
+ }   
+}
