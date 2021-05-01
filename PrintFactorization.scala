52e9ad27df736818c4b0d20271b3f4a25cfab886
Tomoya Kobayashi <tomocha.marika@gmail.com>
2021-05-01 15:47:16 +0900

    fixed middle level

--- PrintFactorization.scala
+++ PrintFactorization.scala
@@ -1,5 +1,3 @@
-object PrintFactorization {
-    def printFactorization() {
-        println(Seq(32, 25, 90, 510510).map(Factorization.factorization))
-    }
+object PrintFactorization extends App {
+    Seq(32, 25, 90, 510510).map(Factorization.factorization).map(println)
 }
